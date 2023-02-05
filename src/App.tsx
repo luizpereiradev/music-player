1ff5dc5ea1b3383a1f14094029b435ae591a797d
Luiz Felipe <luckpecontato@gmail.com>
2023-02-05 14:29:11 -0300

    feat: routes done

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,24 @@
 import Aside from "./components/Aside";
 import Player from "./components/Player";
 import Test from "./components/Test";
+import { BrowserRouter, Route, Routes  } from "react-router-dom";
+import Home from "./components/Home";
+import Library from "./components/Library";
+import Album from "./components/Album";
 function App() {
   return (
+  <BrowserRouter>
     <div className="flex">
         <Aside/>
-        <Test/>
         <Player/>
+    <Routes>
+      <Route element={<Home/>} path="/"/>
+      <Route element={<Test/>} path="/search"/>
+      <Route element={<Library/>} path="/library"/>
+      <Route element={<Album />} path="/album/:id" />
+    </Routes>
     </div>
+  </BrowserRouter>
   );
 }
 
